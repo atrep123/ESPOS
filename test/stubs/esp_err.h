5e5c123f88120b49ed23338d6d477fb0261e8c83
atrep123 <atrep.filip1@gmail.com>
2025-11-24 05:00:59 +0100

    Remove tests and dev configs for app-only setup

--- test/stubs/esp_err.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-typedef int esp_err_t;
-
-#ifndef ESP_OK
-#define ESP_OK 0
-#endif
--- test/test_ui_render_swbuf/app_main.c
+++
@@ -1,4 +0,0 @@
-// Minimal app_main to satisfy ESP-IDF linker during PlatformIO tests
-void app_main(void) {
-    // Intentionally empty; test code here uses native-style mains only
-}
